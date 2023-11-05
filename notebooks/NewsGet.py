663dc3af61b54a8ea2579303ac6a7cfb776b5b6c
宋保贤 <77472835+slience-me@users.noreply.github.com>
2023-11-05 22:14:59 +0800

    first commit

---
+++ notebooks/NewsGet.py
@@ -0,0 +1,192 @@
+import requests
+import bs4
+import os
+import datetime
+import time
+
+
+def fetch_url(url):
+    """
+        功能：访问 url 的网页，获取网页内容并返回
+    :param url: 目标网页的 url
+    :return: 目标网页的 html 内容
+    """
+    headers = {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
+    }
+
+    r = requests.get(url, headers=headers)
+    r.raise_for_status()
+    r.encoding = r.apparent_encoding
+    return r.text
+
+
+def get_page_list(year, month, day):
+    """
+        功能：获取当天报纸的各版面的链接列表
+    :param year: 年 改变年月日拼接成需要爬取的url
+    :param month: 月
+    :param day: 日
+    :return: 返回当天报纸的各版面的链接列表
+    """
+    url = 'http://paper.people.com.cn/rmrb/html/' + year + '-' + month + '/' + day + '/nbs.D110000renmrb_01.htm'
+    html = fetch_url(url)
+    bs_obj = bs4.BeautifulSoup(html, 'html.parser')
+    temp = bs_obj.find('div', attrs={'id': 'pageList'})
+    if temp:
+        page_list = temp.ul.find_all('div', attrs={'class': 'right_title-name'})
+    else:
+        page_list = bs_obj.find('div', attrs={'class': 'swiper-container'}).find_all('div',
+                                                                                     attrs={'class': 'swiper-slide'})
+    link_list = []
+    for page in page_list:
+        link = page.a["href"]
+        url = 'http://paper.people.com.cn/rmrb/html/' + year + '-' + month + '/' + day + '/' + link
+        link_list.append(url)
+    return link_list
+
+
+def get_title_list(year, month, day, pageUrl):
+    """
+        功能：获取报纸某一版面的文章链接列表
+    :param year: 年
+    :param month: 月
+    :param day: 日
+    :param pageUrl: 该版面的链接
+    :return:
+    """
+    html = fetch_url(pageUrl)
+    bs_obj = bs4.BeautifulSoup(html, 'html.parser')
+    temp = bs_obj.find('div', attrs={'id': 'titleList'})
+    if temp:
+        title_list = temp.ul.find_all('li')
+    else:
+        title_list = bs_obj.find('ul', attrs={'class': 'news-list'}).find_all('li')
+    link_list = []
+
+    for title in title_list:
+        temp_list = title.find_all('a')
+        for temp in temp_list:
+            link = temp["href"]
+            if 'nw.D110000renmrb' in link:
+                url = 'http://paper.people.com.cn/rmrb/html/' + year + '-' + month + '/' + day + '/' + link
+                link_list.append(url)
+    return link_list
+
+
+def get_content(html):
+    """
+        功能：解析 HTML 网页，获取新闻的文章内容
+    :param html: html 网页内容
+    :return: 标题+内容
+    """
+    bs_obj = bs4.BeautifulSoup(html, 'html.parser')
+
+    # 获取文章 标题
+    title = bs_obj.h3.text + '\n' + bs_obj.h1.text + '\n' + bs_obj.h2.text + '\n'
+    # print(title)
+
+    # 获取文章 内容
+    p_list = bs_obj.find('div', attrs={'id': 'ozoom'}).find_all('p')
+    content = ''
+    for p in p_list:
+        content += p.text + '\n'
+    # print(content)
+
+    resp = title + content
+    return resp
+
+
+def save_file(content, path, filename):
+    """
+        功能：将文章内容 content 保存到本地文件中
+    :param content: 要保存的内容
+    :param path: 路径
+    :param filename: 文件名
+    :return:
+    """
+    # 如果没有该文件夹，则自动生成
+    if not os.path.exists(path):
+        os.makedirs(path)
+        # print('爬取到文章，正在下载中...')
+
+    # 保存文件
+    with open(path + filename, 'w', encoding='utf-8') as f:
+        f.write(content)
+    # print('爬取到文章，正在下载中...')
+    # print('文章已写入：' + path)
+
+
+def download_rmrb(year, month, day, destdir):
+    """
+        功能：爬取《人民日报》网站某年,某月,某日的新闻内容，并保存在指定目录下
+    :param year: 年
+    :param month: 月
+    :param day: 日
+    :param destdir: 文件保存的根目录
+    :return:
+    """
+    page_list = get_page_list(year, month, day)
+    for page in page_list:
+        title_list = get_title_list(year, month, day, page)
+        for url in title_list:
+            # 获取新闻文章内容
+            html = fetch_url(url)
+            content = get_content(html)
+
+            # 生成保存的文件路径及文件名
+            temp = url.split('_')[2].split('.')[0].split('-')
+            page_no = temp[1]
+            title_no = temp[0] if int(temp[0]) >= 10 else '0' + temp[0]
+            path = destdir + '/' + year + month + day + '/'
+            file_name = year + month + day + '-' + page_no + '-' + title_no + '.txt'
+
+            # 保存文件
+            save_file(content, path, file_name)
+
+
+def gen_dates(b_date, days):
+    day = datetime.timedelta(days=1)
+    for i in range(days):
+        yield b_date + day * i
+
+
+def get_date_list(beginDate, endDate):
+    """
+        获取日期列表
+    :param beginDate: 开始日期
+    :param endDate: 结束日期
+    :return:
+    """
+    start = datetime.datetime.strptime(beginDate, "%Y%m%d")
+    end = datetime.datetime.strptime(endDate, "%Y%m%d")
+
+    data = []
+    for d in gen_dates(start, (end - start).days):
+        data.append(d)
+    # return: 返回开始日期和结束日期之间的日期列表
+    return data
+
+    # 主函数：程序入口
+
+
+def get_news():
+    # 输入起止日期，爬取之间的新闻
+    print('---文章爬取系统---')
+    beginDate = input('请输入开始日期(格式如20231101):')
+    endDate = input('请输入结束日期(格式如20231101):')
+    data = get_date_list(beginDate, endDate)
+
+    for d in data:
+        year = str(d.year)
+        month = str(d.month) if d.month >= 10 else '0' + str(d.month)
+        day = str(d.day) if d.day >= 10 else '0' + str(d.day)
+        # 爬取后文章t统一存到这个文件夹,没有会自动创建
+        destdir = "../data"
+
+        download_rmrb(year, month, day, destdir)
+        print('---文章爬取系统---')
+        print("爬取文章完成！")
+        print('爬取文章时间为：' + year + '/' + month + '/' + day + '的文章已成功写入文件夹中！')
+        print('---文章爬取系统---')
---
+++ notebooks/demo.py
@@ -0,0 +1,177 @@
+import multiprocessing
+import os
+import time
+
+import jieba
+from NewsGet import get_news
+from random import sample
+import re
+from zhon.hanzi import punctuation as puncZH
+from string import punctuation as puncEN
+import pandas as pd
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+# 全局配置
+encoding = 'utf-8'
+
+
+def get_news_path():
+    news_path = []
+    for dirname, _, filenames in os.walk('../data'):
+        for filename in filenames:
+            news_path.append(os.path.join(dirname, filename).replace('\\', '/'))
+    print("新闻总数为: ", len(news_path))
+    # print(news_path)
+    return news_path
+
+
+def removePunctuation(s: str):
+    s = re.sub(r"[%s]+" % puncZH, "", s)
+    s = re.sub(r"[%s]+" % puncEN, "", s)
+    s = re.sub(r"[\d\n\t\r]+", "", s)
+    return s
+
+
+def hasPunctuation(s: str):
+    f = False
+    f |= bool(re.match(r"[%s]+" % puncZH, s))
+    f |= bool(re.match(r"[%s]+" % puncEN, s))
+    f |= bool(re.match(r"[\d\n\t\r]+", s))
+    return f
+
+
+def getWords(news: str):
+    split_news = jieba.lcut_for_search(news)
+    removed_split_news = []
+    for text in split_news:
+        if hasPunctuation(text) is False:
+            removed_split_news.append(text)
+    return removed_split_news
+
+
+def get_words_count(news_path):
+    words = []
+    for path in sample(news_path, k=50):
+        news = open(path, 'r', encoding=encoding).read()
+        words += getWords(news)
+    print('词语数量: ', len(words))
+    return words
+
+
+def get_unique_words_count(words):
+    word_count = {}
+    for word in words:
+        word_count[word] = word_count.get(word, 0) + 1
+    print('去重词语的个数：', len(word_count))
+    return word_count
+
+
+def get_stop_words():
+    stopwords = []
+    for dirname, _, filenames in os.walk('./stopwords-master'):
+        for filename in filenames:
+            with open(os.path.join(dirname, filename)) as f:
+                if filename.endswith('txt'):
+                    stopwords += list(f.read().split())
+
+    stopwords.append('年')
+    stopwords.append('月')
+    stopwords.append('日')
+    stopwords.append('上')
+    stopwords.append('中')
+    stopwords.append('本报')
+    stopwords.append('的')
+    stopwords.append('和')
+    stopwords.append('在')
+    stopwords.append('为')
+    stopwords.append('了')
+    stopwords.append('\xa0')
+
+    stopwords = list(set(stopwords))
+    print('统计停止词总数: ', len(stopwords))
+    return stopwords
+
+
+def data_handle_word_ds(word_count):
+    word_ds = pd.DataFrame(word_count, index=[0])
+    word_ds = word_ds.T
+    word_ds.columns = ['count']
+    word_ds.sort_values(by=['count'], ascending=False, inplace=True)
+    return word_ds
+
+
+def word_count_remove_stopword(word_count):
+    stopwords = get_stop_words()
+    for stopword in stopwords:
+        if stopword in word_count:
+            word_count.pop(stopword)
+    print('不带停止语的唯一单词总数: ', len(word_count))
+    return word_count
+
+
+def timer_process():
+    start_time = time.time()
+    while True:
+        current_time = time.time()
+        elapsed_time = current_time - start_time
+        print(f"Elapsed Time: {int(elapsed_time)} seconds", end='\r')
+        time.sleep(1)
+
+
+def draw_wordcloud(word_count):
+    start_time = time.time()
+    print("begin to init")
+    wc = WordCloud(font_path='../Fonts/方正正中黑简体.ttf', width=9000, height=6000, background_color="red",
+                   max_words=1000,
+                   color_func=lambda *args, **kwargs: (255, 255, 0))
+    print("begin to generate images")
+    timer = multiprocessing.Process(target=timer_process)
+    timer.start()
+    wc.generate_from_frequencies(word_count)
+    # 主进程执行完毕后，关闭计时进程
+    timer.terminate()
+    timer.join()
+    print("begin to save images")
+    wc.to_file("People's Daily WordCloud Zh_1.png")
+    plt.figure(figsize=(32, 16))
+    plt.imshow(plt.imread("./People's Daily WordCloud Zh_1.png"))
+    plt.axis("off")
+    plt.show()
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"代码块执行时间：{elapsed_time} 秒")
+
+
+def get_current_data_list(global_path):
+    local_data = []
+    for dirname, _, filenames in os.walk(global_path):
+        if global_path == dirname:
+            continue  # 跳过根路径
+        local_data.append(int(dirname.replace(global_path, '')))
+    local_data.sort()
+    print('current exist data:', local_data)
+
+
+def get_new_data(global_path):
+    get_current_data_list(global_path)
+    while (True):
+        flag = input("add new data?Y/N")
+        if flag == 'Y':
+            get_news()
+            break
+        elif flag == 'N':
+            print('do not add new data!')
+            break
+        else:
+            print('please input Y or N!')
+
+
+if __name__ == '__main__':
+    global_path = '../data/'
+    get_new_data(global_path)
+    news_path = get_news_path()
+    words = get_words_count(news_path)
+    word_count = get_unique_words_count(words)
+    word_count = word_count_remove_stopword(word_count)
+    draw_wordcloud(word_count)
